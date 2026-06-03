8ca98bb042f52ec8807717a3d19a2698a069ae70
RafaelSalgueiro58 <rafaelsalgueiro58@gmail.com>
2026-06-03 15:37:11 +0200

    Uncomment line so that when setting the OPD by the user, OPD_no_pupil is also automatically set to OPD.

--- OOPAO/Source.py
+++ OOPAO/Source.py
@@ -208,7 +208,7 @@ class Source:
     def OPD(self, val):
         if val is not None:
             self._OPD = np.array(val)
-            # self._OPD_no_pupil = np.array(val)
+            self._OPD_no_pupil = np.array(val)
         else:
             self._OPD = None
             self._OPD_no_pupil = None
